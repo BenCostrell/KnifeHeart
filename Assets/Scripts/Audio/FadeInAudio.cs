19355c14e03165aa26d15382b122fd86c528f5af
Ben Costrell <bencostrell@gmail.com>
2017-05-01 19:23:49 -0400

    (totally untested) structure for music

--- Assets/Scripts/Audio/FadeInAudio.cs
+++ Assets/Scripts/Audio/FadeInAudio.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FadeInAudio : MonoBehaviour {
+public class FadeInAudio : Task {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float timeElapsed;
+    private float duration;
+    private AudioSource source;
+
+    public FadeInAudio(AudioSource src, float dur)
+    {
+        duration = dur;
+        source = src;
+    }
+
+    protected override void Init()
+    {
+        timeElapsed = 0;
+        source.volume = 0;
+    }
+
+    internal override void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        source.volume = Mathf.Lerp(0, 1, Easing.QuadEaseIn(timeElapsed / duration));
+
+        if (timeElapsed >= duration) SetStatus(TaskStatus.Success);
+    }
+
 }
--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -12,18 +12,26 @@ public class MusicManager : MonoBehaviour {
     private AudioSource baseSource;
     private AudioSource[] fightSources;
 
+    public float fadeInTime;
+    public float fadeOutTime;
+
+    private List<AudioSource> currentActiveSources;
+
 	// Use this for initialization
 	void Awake () {
         Services.EventManager.Register<SceneTransition>(OnSceneTransition);
+        Services.EventManager.Register<FightAdvance>(OnFightAdvance);
 	}
 	
     void Start()
     {
+        currentActiveSources = new List<AudioSource>();
         titleSource = InitializeAudio(titleTrack);
         baseSource = InitializeAudio(baseTrack);
-        foreach(AudioClip clip in fightTracks)
+        fightSources = new AudioSource[fightTracks.Length];
+        for (int i = 0; i < fightTracks.Length; i++)
         {
-
+            fightSources[i] = InitializeAudio(fightTracks[i]);
         }
     }
 
@@ -37,14 +45,26 @@ public class MusicManager : MonoBehaviour {
         switch (e.sceneName)
         {
             case "TitleScreen":
+                StartPlayingTrack(titleSource);
                 break;
             case "VisualNovelScene":
+                StartPlayingTrack(baseSource);
                 break;
             case "FightScene":
+                if (Services.FightScene.roundNum != 3) StartPlayingTrack(fightSources[Services.FightScene.roundNum - 1]);
+                else {
+                    StartFinalFightTracks();
+                    FadeInTrack(fightSources[2]);
+                }
                 break;
             default:
                 break;
         }
+    }
+
+    void OnFightAdvance(FightAdvance e)
+    {
+        FadeInTrack(fightSources[e.roundNum - 1]);
     }
 
     AudioSource InitializeAudio(AudioClip clip)
@@ -52,11 +72,46 @@ public class MusicManager : MonoBehaviour {
         AudioSource source = gameObject.AddComponent<AudioSource>();
         source.clip = clip;
         source.playOnAwake = false;
+        source.loop = true;
         return source;
     }
 
-    void StartPlayingTrack()
+    void StartPlayingTrack(AudioSource source)
     {
+        ActionTask playTrack = new ActionTask(source.Play);
+        FadeInAudio fadeIn = new FadeInAudio(source, fadeInTime);
+        playTrack.Then(fadeIn);
 
+        if (currentActiveSources.Count > 0)
+        {
+            FadeOutAudio fadeOut = null;
+            for (int i = currentActiveSources.Count - 1; i >= 0; i--)
+            {
+                fadeOut = new FadeOutAudio(currentActiveSources[i], fadeOutTime);
+                Services.TaskManager.AddTask(fadeOut);
+                currentActiveSources.Remove(currentActiveSources[i]);
+            }
+            fadeOut.Then(playTrack);
+        }
+        else
+        {
+            Services.TaskManager.AddTask(playTrack);
+        }
+        currentActiveSources.Add(source);
+    }
+
+    void FadeInTrack(AudioSource source)
+    {
+        Services.TaskManager.AddTask(new FadeInAudio(source, fadeInTime));
+    }
+
+    void StartFinalFightTracks()
+    {
+        for (int i = 2; i < 4; i++)
+        {
+            fightSources[i].volume = 0;
+            fightSources[i].Play();
+            currentActiveSources.Add(fightSources[i]);
+        }
     }
 }
