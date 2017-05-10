079bad1fb0f688563adfef2b534fd909c3087807
Ben Costrell <bencostrell@gmail.com>
2017-05-10 16:50:10 -0400

    restructured pull w/ a state machine, seems to work, not sure if buggy

--- Assets/Scripts/FightStuff/Abilities/Pull.cs
+++ Assets/Scripts/FightStuff/Abilities/Pull.cs
@@ -7,26 +7,29 @@ public class Pull : Attack {
 	public float distanceToPullTo;
 	public float hitstun;
     public float positionOffset;
-    private TaskManager pullTaskManager;
-    private bool pullFired;
+    private FSM<Pull> stateMachine;
+    [HideInInspector]
+    public Player hookedPlayer;
+    [HideInInspector]
+    public Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-        pullTaskManager = new TaskManager();
-        pullFired = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        pullTaskManager.Update();
-        if (pullFired && !pullTaskManager.hasActiveTasks()) DestroyPull();
-	}
+        stateMachine.Update();
+    }
 
 	public override void Init(GameObject player){
 		base.Init (player);
-	}
+        rb = GetComponent<Rigidbody2D>();
+        stateMachine = new FSM<Pull>(this);
+        stateMachine.TransitionTo<PreFire>();
+    }
 
-	protected override Vector3 GetDirectionHit(GameObject playerHit){
+    protected override Vector3 GetDirectionHit(GameObject playerHit){
 		return GetComponent<Rigidbody2D> ().velocity.normalized;
 	}
 
@@ -38,17 +41,17 @@ public class Pull : Attack {
     public override void SetActive()
     {
         base.SetActive();
+        stateMachine.TransitionTo<Extending>();
+    }
 
+    public void BeginToExtend()
+    {
         float angle = parentPlayer.GetComponent<Player>().effectiveRotation;
         Vector3 direction = new Vector3(-Mathf.Cos(angle * Mathf.Deg2Rad), -Mathf.Sin(angle * Mathf.Deg2Rad));
 
         transform.rotation = Quaternion.Euler(0, 0, angle);
         transform.localPosition += positionOffset * direction;
         GetComponent<Rigidbody2D>().velocity = speed * direction;
-
-        PullTask pullTask = new PullTask(parentPlayer.GetComponent<Player>(), this);
-        pullTaskManager.AddTask(pullTask);
-        pullFired = true;
     }
 
     public override void OnCastFinish()
@@ -59,6 +62,99 @@ public class Pull : Attack {
     public void DestroyPull()
     {
         Destroy(gameObject);
+    }
+
+
+    // STATES //
+
+    private class PullState : FSM<Pull>.State
+    {
+        public override void OnEnter()
+        {
+            Services.EventManager.Register<AbilityEnded>(OnAbilityEnded);
+        }
+
+        public override void OnExit()
+        {
+            Services.EventManager.Unregister<AbilityEnded>(OnAbilityEnded);
+        }
+
+        protected virtual void OnAbilityEnded(AbilityEnded e)
+        {
+            if (e.ability == Context)
+            {
+                if (Context.parentPlayer.GetComponent<Player>().stunned) EndPull();
+            }
+        }
+
+        protected virtual void EndPull()
+        {
+            Services.EventManager.Unregister<AbilityEnded>(OnAbilityEnded);
+            Context.DestroyPull();
+        }
+    }
+
+    private class PreFire : PullState
+    {
+    }
+
+    private class Extending : PullState
+    {
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            Context.BeginToExtend();
+            Services.EventManager.Register<PlayerHooked>(StartRetracting);
+        }
+
+        void StartRetracting(PlayerHooked e)
+        {
+            Context.hookedPlayer = e.hookedPlayer;
+            TransitionTo<Retracting>();
+            Services.EventManager.Unregister<PlayerHooked>(StartRetracting);
+        }
+
+        protected override void OnAbilityEnded(AbilityEnded e)
+        {
+            if (e.ability == Context)
+            {
+                EndPull();
+            }
+        }
+
+        protected override void EndPull()
+        {
+            Services.EventManager.Unregister<PlayerHooked>(StartRetracting);
+            base.EndPull();
+        }
+    }
+
+    private class Retracting : PullState
+    {
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            Context.hookedPlayer.StopListeningForInput();
+        }
+
+        public override void Update()
+        {
+            Context.rb.velocity = Context.speed * 
+                (Context.parentPlayer.transform.position - Context.hookedPlayer.transform.position).normalized;
+            Context.hookedPlayer.rb.MovePosition(Context.transform.position);
+            if (InPosition())
+            {
+                Context.hookedPlayer.Stun(Context.hitstun);
+                Context.parentPlayer.GetComponent<Player>().StartListeningForInput();
+                EndPull();
+            }
+        }
+
+        bool InPosition()
+        {
+            return (Vector3.Distance(Context.parentPlayer.transform.position, Context.hookedPlayer.transform.position) 
+                <= Context.distanceToPullTo);
+        }
     }
 
 }
