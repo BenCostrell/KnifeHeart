bc47f11b5caf543e3fd76f533c9d4720ed17a5c9
DavidFrisch <dmf463@nyu.edu>
2017-05-11 18:59:58 -0400

    got the FMOD working

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMOD;
+using FMODUnity;
 
-public class MusicManager : MonoBehaviour {
+public class MusicManager : MonoBehaviour
+{
 
     public AudioClip titleTrack;
     public AudioClip baseTrack;
@@ -28,9 +31,14 @@ public class MusicManager : MonoBehaviour {
     public AudioClip fightinWordsSwirl;
     public AudioClip fightTransition;
 
+    [FMODUnity.EventRef]
+    public string FinalBattle;
+    FMOD.Studio.EventInstance finaleBattle;
+
 
     // Use this for initialization
-    public void Init () {
+    public void Init()
+    {
         currentActiveSources = new List<AudioSource>();
         titleSource = InitializeAudio(titleTrack);
         baseSource = InitializeAudio(baseTrack);
@@ -41,16 +49,17 @@ public class MusicManager : MonoBehaviour {
         }
         Services.EventManager.Register<SceneTransition>(OnSceneTransition);
         Services.EventManager.Register<FightAdvance>(OnFightAdvance);
-	}
-	
+    }
+
     void Start()
     {
     }
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnSceneTransition(SceneTransition e)
     {
@@ -63,11 +72,12 @@ public class MusicManager : MonoBehaviour {
                 StartPlayingTrack(baseSource);
                 break;
             case "FightScene":
-                //StartPlayingTrack(fightSources[Services.FightScene.roundNum - 1]);
-                //if(Services.FightScene.roundNum == 3)
-                //{
-                //    StartRooftop();
-                //}
+                StartPlayingTrack(fightSources[Services.FightScene.roundNum - 1]);
+                if (Services.FightScene.roundNum == 3)
+                {
+                    finaleBattle = FMODUnity.RuntimeManager.CreateInstance(FinalBattle);
+                    finaleBattle.start();
+                }
                 break;
             default:
                 break;
@@ -78,10 +88,14 @@ public class MusicManager : MonoBehaviour {
     {
         //FadeInTrack(fightSources[e.roundNum - 1]);
         StartPlayingTrack(fightSources[e.roundNum - 1]);
-        //if(fightSources[e.roundNum = 4])
-        //{
-        //    StartParkingLot();
-        //}
+        if (e.roundNum == 4)
+        {
+            StartParkingLot();
+        }
+        if (e.roundNum == 5)
+        {
+            StartHellLevel();
+        }
     }
 
     AudioSource InitializeAudio(AudioClip clip)
@@ -150,16 +164,22 @@ public class MusicManager : MonoBehaviour {
         currentActiveSources.Remove(baseSource);
     }
 
-    //public void StartRooftop()
-    //{
-    //    this.GetComponent<StudioEventEmitter>().enabled = true;   
-    //}
+    public void StartRooftop()
+    {
+        this.GetComponent<StudioEventEmitter>().enabled = true;
+    }
 
-    //public void StartParkingLot()
-    //{
-    //    FMOD.Studio.EventInstance parkingLot = FMODUnity.RuntimeManager.CreateInstance("Final Battle");
-    //    parkingLot.setParameterValue("Level 2", 1.0f);
-    //    parkingLot.start();
-    //}
+    public void StartParkingLot()
+    {
+        //finaleBattle.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        //FMOD.Studio.EventInstance parkingLot = RuntimeManager.CreateInstance(FinalBattle);
+        finaleBattle.setParameterValue("Level 2", .51f);
+        //parkingLot.start();
+    }
+
+    public void StartHellLevel()
+    {
+        finaleBattle.setParameterValue("Level 3", .51f);
+    }
 
 }
