8e35cce4c83c7be8181c15f869bd2c62315807ce
Unknown <andrewpetersell@gmail.com>
2017-06-14 14:30:09 -0400

    Blink Effect Changes

--- Assets/Scripts/FightStuff/Abilities/Blink.cs
+++ Assets/Scripts/FightStuff/Abilities/Blink.cs
@@ -15,30 +15,35 @@ public class Blink : Ability
         base.Init(player);
         parentPlayer.isInvulnerable = true;
         PlayTeleportEffect(true);
+		transform.position = CalculateTargetPosition ();
+		PlayTeleportEffect (false);
     }
+
+	Vector3 CalculateTargetPosition(){
+		Feet feet = parentPlayer.GetComponentInChildren<Feet>();
+		float angle = parentPlayer.effectiveRotation;
+		Vector3 direction = new Vector3(-Mathf.Cos(angle * Mathf.Deg2Rad), -Mathf.Sin(angle * Mathf.Deg2Rad)).normalized;
+		Vector3 targetPosition;
+		RaycastHit2D hit;
+		hit = Physics2D.Raycast(feet.transform.position, direction, distance, stageBoundary);
+		if (hit)
+		{
+			float distanceToBlink = Mathf.Max(hit.distance - stopShortDistance, 0f);
+			targetPosition = parentPlayer.transform.position + distanceToBlink * direction;
+		}
+		else
+		{
+			targetPosition = parentPlayer.transform.position + distance * direction;
+		}
+		return targetPosition;
+	}
 
     void Teleport()
     {
         parentPlayer.isInvulnerable = false;
         parentPlayer.rb.velocity = Vector3.zero;
-        Feet feet = parentPlayer.GetComponentInChildren<Feet>();
-        float angle = parentPlayer.effectiveRotation;
-        Vector3 direction = new Vector3(-Mathf.Cos(angle * Mathf.Deg2Rad), -Mathf.Sin(angle * Mathf.Deg2Rad)).normalized;
-        Vector3 targetPosition;
-        RaycastHit2D hit;
-        hit = Physics2D.Raycast(feet.transform.position, direction, distance, stageBoundary);
-        if (hit)
-        {
-            float distanceToBlink = Mathf.Max(hit.distance - stopShortDistance, 0f);
-            targetPosition = parentPlayer.transform.position + distanceToBlink * direction;
-        }
-        else
-        {
-            targetPosition = parentPlayer.transform.position + distance * direction;
-        }
+		Vector3 targetPosition = CalculateTargetPosition (); 
         parentPlayer.rb.MovePosition(targetPosition);
-        transform.position = targetPosition;
-        PlayTeleportEffect(false);
     }
 
     void PlayTeleportEffect(bool goingOut)
